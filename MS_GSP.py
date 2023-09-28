e7e4fcc9f38cecc9aed243b5aa69f8dbd7e01218
Chris Grams <cgrams2@uic.edu>
2023-09-28 00:08:53 -0500

    Minor readparm fix, added init_pass() and filter()

--- MS_GSP.py
+++ MS_GSP.py
@@ -32,7 +32,7 @@ def readdata():
     return data
     
 def readparam():
-    mis = [] #holds our minsups
+    mis = {} #holds our minsups
     temp = [] #temp list #1
     sdc = 0
     #print("\nParameters:")
@@ -45,7 +45,7 @@ def readparam():
             item = temp[0][temp[0].find("(")+1:temp[0].find(")")]
             #item = re.findall(r'\(.*?\)', temp[0])
             value = float(temp[1].rstrip("\n"))
-            mis.append([item,value])
+            mis[int(item)] = value
             #print(item,"-",value)
         #print(line)
         else:
@@ -55,10 +55,49 @@ def readparam():
     return mis,sdc
 
 
-data = readdata()
-print("Data:\n",data)
-print("\n************\n")
-mis, sdc = readparam()
-print("MIS:\n",mis)
-print("\n************\n")
-print("SDC = ",sdc)
+def init_pass(S):
+    '''
+    Go through dataset "S" once to calculate support count of individual items.
+    :param S:
+    :return: A dictionary with key being the item, value being count.
+    '''
+    ret = {}
+    for itemset in S:
+        for sequence in itemset:
+            for item in sequence:
+                if item in ret:
+                    ret[item] += 1
+                else:
+                    ret[item] = 1
+    return ret
+
+def filter(C, mis, num_sequences):
+    '''
+    Go through candidates "C", filter out those that fall below minsup for that item.
+    F <- {<F> | for f in C, f.count >= minsup}
+    :param C: Candidates from init_pass
+    :param mis: list of minsups
+    :num_sequences: number of sequences in S (len(S))
+    :return:
+    '''
+    ret = {}
+    for candidate in C:
+        val = C[candidate]/num_sequences # f.count / n
+        if val >= mis[candidate]:
+            ret[candidate] = C[candidate]
+
+    return ret
+# GSP algorithm:
+def GSP(S, mis):
+    C = init_pass(S)
+    F = filter(C, mis, len(S))
+
+if __name__ == "__main__":
+    data = readdata()
+    print("Data:\n",data)
+    print("\n************\n")
+    mis, sdc = readparam()
+    print("MIS:\n",mis)
+    print("\n************\n")
+    print("SDC = ",sdc)
+    GSP(data, mis)
