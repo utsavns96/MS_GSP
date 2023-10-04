1926b194475dfb6bb866c263507680c021276304
Chris Grams <chrisagrams@gmail.com>
2023-10-04 05:50:57 -0500

    SPM

--- candidate_gen.py
+++ candidate_gen.py
@@ -60,7 +60,6 @@ def flatten_subsequence(t):
             l.append(i)
     return l
 
-
 def join_step(s1, s2):
     new_tup = ()
     # if the subsequence obtained by dropping the first item of s1
@@ -122,6 +121,7 @@ def prune_step(Fk_1,Ck, mis):
                 seq = seq_t
             #1. find minMIS and item with minMIS
             min_mis_seq_item = seq[0]
+            print(c)
             for item in seq:
                 if(mis[item]<mis[min_mis_seq_item]):
                     min_mis_seq_item = item
@@ -140,7 +140,7 @@ def prune_step(Fk_1,Ck, mis):
                     del temp_c[index_seq][index_i]
                     temp_c = list(filter(None, temp_c))
                     temp_list.append(list(temp_c))
-        print(temp_list)
+        # print(temp_list)
         temp = 0
         #3. iterate through them and compare with Fk_1
         #first convert Fk_1 to list of lists
@@ -231,28 +231,31 @@ def mscandidate_gen_SPM(Fk_1, mis):
     # Ck = join_step(Fk_test)
     for s1 in Fk_1:
         s1_first_mis = mis[flatten_subsequence(s1)[0]] if isinstance(s1[0], tuple) else mis[s1[0]]
-        s1_first_smallest = None
+        s1_first_smallest = True
         # check for condition 1
         for index,i in enumerate(s1[1:]):
             mis_i = mis[flatten_subsequence(s1)[0]] if isinstance(i, tuple) else mis[i]
-            if index < len(s1[1:]) -1 and mis_i<s1_first_mis:
+            # if index < len(s1[1:]) -1 and mis_i<s1_first_mis:
+            #     s1_first_smallest = False
+            # elif index == len(s1[1:]) -1 and mis_i<s1_first_mis:
+            #     s1_first_smallest = True
+            if s1_first_mis >= mis_i:
                 s1_first_smallest = False
-            elif index == len(s1[1:]) -1 and mis_i<s1_first_mis:
-                s1_first_smallest = True
-
         for s2 in Fk_1:
-            s2_last_smallest = None
+            s2_last_smallest = False
             s2_last_mis = mis[flatten_subsequence(s2)[-1]] if isinstance(s2[-1:], tuple) else mis[s2[-1:]]
             
             # check for condition 2
-            if (not s1_first_smallest):
+            if not s1_first_smallest:
+                s2_last_smallest = True
                 for index, i in enumerate(s2[:-1]):
                     mis_i = mis[flatten_subsequence(s2)[-1]] if isinstance(i, tuple) else mis[i]
-                    if index < len(s2[:-1]) -1 and mis_i<s2_last_mis:
+                    # if index < len(s2[:-1]) -1 and mis_i<s2_last_mis:
+                    #     s2_last_smallest = False
+                    # elif index == len(s2[:-1]) -1 and mis_i<s2_last_mis:
+                    #     s2_last_smallest = True
+                    if s2_last_mis >= mis_i:
                         s2_last_smallest = False
-                    elif index == len(s2[:-1]) -1 and mis_i<s2_last_mis:
-                        s2_last_smallest = True
-
             # condition 1: if the MIS value of the first item in a sequence (denoted by s1) is < the MIS value of every other item in s1
             if(s1_first_smallest):
                 t1 = list(s1)
@@ -265,17 +268,35 @@ def mscandidate_gen_SPM(Fk_1, mis):
                         #print("Ck: {}".format(Ck))
                         s1_last_item = flatten_subsequence(s1[-1:]) if isinstance(s1[-1:], tuple) else s1[-1:]
                         s2_last_item = flatten_subsequence(s2[-1:]) if isinstance(s2[-1:], tuple) else s2[-1:]
-                        if Length(s1) == 2 and Size(s1)==2 and s2_last_item > s1_last_item: 
+                        if Length(s1) == 2 and Size(s1) == 2 and s2_last_item > s1_last_item:
                             c2 = list(s1)
                             c2.append(flatten_subsequence(s2[-1]))
-                    elif (Length(s1) == 1 and Size(s1)==2 and s2_last_item > s1_last_item) or (Length(s1)>2):
-                        c2 = list(s1)
-                        c2.append(flatten_subsequence(s2[-1]))
+                            Ck.add(c2)
+                        elif (Length(s1) == 2 and Size(s1) == 1 and s2_last_item > s1_last_item) or (Length(s1)>2):
+                            c2 = list(s1)
+                            c2.append(flatten_subsequence(s2[-1]))
+                            Ck.add(c2)
+                        Ck.add(c1)
                     
             # condition 2: if the MIS value of the last item in a sequence (denoted by s2) is < the MIS value of every other item in s2
             elif (s2_last_smallest):
-                print("s2_last_smallest")
-                break
+                t2 = list(s2)
+                t2.pop(1)
+                if t2 == list(s1)[:-1] and s1_first_mis>s2_last_mis:
+                    if isinstance(s1[-1], tuple):
+                        c2 = list(s2)
+                        c2.append(s2[-1])
+                        s1_last_item = flatten_subsequence(s1[-1:]) if isinstance(s1[-1:], tuple) else s1[-1:]
+                        s2_last_item = flatten_subsequence(s2[-1:]) if isinstance(s2[-1:], tuple) else s2[-1:]
+                        if Length(s2) == 2 and Size(s2) == 2 and s1_last_item > s2_last_item:
+                            c1 = list(s2)
+                            c1.append(flatten_subsequence(s1[-1]))
+                            Ck.add(c1)
+                        elif Length(s2) == 2 and Size(s2) == 1 and s1_last_item > s2_last_item or Length(s2) > 2:
+                            c1 = list(s2)
+                            c1.append(flatten_subsequence(s1[-1]))
+                            Ck.add(c1)
+                        Ck.add(c2)
 
             # condtition 3: default case
             else:
